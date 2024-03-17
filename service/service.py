965c14dcb736981aafbf7d23dc7d28b8ad95eb95
unknown <simonas.b@hotmail.com>
2024-03-17 18:59:31 +0200

    more beautiful

--- service/service.py
+++ service/service.py
@@ -1,5 +1,6 @@
 import logging
 from datetime import datetime
+from time import sleep
 
 
 class Service:
@@ -10,15 +11,16 @@ class Service:
         self.log = self._setup_extra_logging_info()
 
 
-
     def run(self):
         self.log.info(f"Service is running at {datetime.now()} with correlation id {self.correlation_id}")
+        sleep(5)
+        self.log.info(f"Service finished running")
 
 
     def _setup_extra_logging_info(self):
         logger = logging.LoggerAdapter(
             logging.getLogger(f"{self.__class__.__name__} from {__name__}"),
-            {
+            extra = {
                 "correlation_id" : self.correlation_id,
                 "username": "dbadmin"
             }
